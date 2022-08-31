a25a30df6db996fbad4b432ee81e49443e8e443f
Diana Nnaemeka <104990762+dianadeycode@users.noreply.github.com>
2022-08-31 17:06:17 +0100

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,5 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
-    maxval = 0
-    maxkey = None
-    for k, v in a_dictionary.items():
-        if v > maxval:
-            maxval = v
-            maxval = k
-    return maxkey
+    return max(a_dictionary)
