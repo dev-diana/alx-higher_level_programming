566d90a3386d079c32ef6396d03d71391d395708
Diana Chisom Nnaemeka <104990762+dianadeycode@users.noreply.github.com>
2022-09-12 22:00:51 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
 
-def safe_print_list_integers(my_list=[], x=0):
-    i, j = 0, 0
-    while i < x:
-        try:
-            print("{:d}".format(my_list[i]), end='')
-            j += 1
-        except (ValueError, TypeError):
-            pass
-        i += 1
-    print()
-    return j
+def safe_print_division(a, b):
+    try:
+        a = a / b
+    except ZeroDivisionError:
+        a = None
+    finally:
+        print("Inside result:", "{}".format(a))
+    return a
